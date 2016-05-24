3d5be566ba5cc78747b20af522897d7fcaacdfc4
Jack Sarick <jacksarick@gmail.com>
2016-05-23 21:26:01 -0400

    Basic auth working

--- verbatim.js
+++ verbatim.js
@@ -1,13 +1,30 @@
 #!/usr/local/bin/node
 
 var net = require('net');
+var config = require("./config.json");
 
-var config = require("config.json")
+function authenticate(data) {
+	// If the user's authentication checks out
+	if (data.user != undefined &&
+		data.pass != undefined &&
+		config.users[data.user] != undefined &&
+		config.users[data.user].pass == data.pass) {
+
+		return config.users[data.user]["tables"];
+	}
+
+	// Else, the user has access to nothing
+	else {
+		return [];
+	}
+}
 
 var server = net.createServer(function(socket) {
+	// Default access is no tables
+	socket.tables = [];
 
-	// Log it to the server output
-	console.log("New socket with " + socket.name);
+	// Log opening to output
+	console.log("Opened new socket");
 
 	// Welcome user to the socket
 	socket.write("{'status':'connected'}");
@@ -15,17 +32,51 @@ var server = net.createServer(function(socket) {
 
 	// When client sends data
 	socket.on('data', function(data) {
-		var msg = data.toString().replace(/[^\d]/g, "");
-		var response = "{'data':" + (msg*socket.name) + "}";
-		console.log(id ": " + msg +" => "+ response)
-		socket.write(response);
+		data = data.toString();
+
+		// Try to convert data to JSON
+		try	{
+			data = JSON.parse(data);
+		}
+
+		// If we fail, let the user know
+		catch(err) {
+			socket.write("{'status': 'error'}");
+			console.log("Request failed")
+		}
+
+		// If user is not authenticated...
+		if(socket.tables.length < 1){
+			socket.tables = authenticate(data);
+
+			// if the can access tables, let them know
+			if (socket.tables.length > 0) {
+				var neat = "['" + socket.tables.join("','") + "']";
+				socket.write("{'status': 'sucess', 'tables': " + neat + "}")
+			}
+
+			// If the user has no tables, bye bye
+			else {
+				socket.end("{'status': 'error'}");
+			}
+		}
+
+		// Else...
+		else {
+			var msg = data.replace(/[^\d]/g, "");
+			var response = "{'data':" + (msg*socket.name) + "}";
+
+			console.log(id + ": " + msg +" => "+ response)
+
+			socket.write(response);
+		}
 	});
 
 
 	// When client leaves
 	socket.on('end', function() {
 		// Log it to the server output
-		console.log("User " + socket.name + " left");
+		console.log("Socket closed");
 	});
 
 	// When socket gets errors
@@ -41,6 +92,6 @@ server.on('error', function(error) {
 });
 
 // Listen on the port
-server.listen(port, function() {
-	console.log("Server listening at localhost on port " + port);
+server.listen(config.port, function() {
+	console.log("Server listening at localhost on port " + config.port);
 });
